5e87ef1444250f4920f41e4b5f41790a1ba25115
Emerson Lacerda <emerson_lacerda001@outlook.com>
2020-07-31 20:12:20 -0300

    Personalização do Site
    Customização dos vídeos mostrados e categorias existentes

--- src/App.js
+++ src/App.js
@@ -40,6 +40,10 @@ function App() {
       <Carousel
         category={dadosIniciais.categorias[5]}
       />      
+      
+      <Carousel
+        category={dadosIniciais.categorias[6]}
+      />            
 
       <Footer />
     </div>
